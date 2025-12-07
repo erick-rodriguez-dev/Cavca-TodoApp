776f4c6819703a16b43be1fa01d6d8c00d71904d
erick-rodrigue-dev <erick.rodriguez1952@gmail.com>
2025-12-07 17:13:02 -0500

    refactor: remove 'Discard Task' button from TaskForm component

--- FrontEnd/src/components/TaskForm.jsx
+++ FrontEnd/src/components/TaskForm.jsx
@@ -53,16 +53,6 @@ export default function TaskForm() {
 
       <div className="flex justify-between gap-4">
         <button
-          type="button"
-          onClick={() => {
-            setTitle('')
-            setDescription('')
-          }}
-          className="w-full cursor-pointer rounded-md border border-red-500 px-4 py-2 font-medium text-red-500 transition-colors hover:bg-red-600 hover:text-white"
-        >
-          Descartar Tarea
-        </button>
-        <button
           type="submit"
           className="w-full cursor-pointer rounded-md bg-green-500 px-4 py-2 font-medium text-white transition-colors hover:bg-green-600"
         >
